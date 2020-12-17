e1fbc6b9fca36be586d7a489f63b8d8b63374006
Somesh Kale <someshk3850@gmail.com>
2020-12-17 17:12:50 -0500

    resolved version issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyCLvzsh9O37AYy9k7FLsELhVsLhPfpNySc",
+    authDomain: "loginapp-c1ae4.firebaseapp.com",
+    databaseURL:'https://loginapp-c1ae4-default-rtdb.firebaseio.com/',
+    projectId: "loginapp-c1ae4",
+    storageBucket: "loginapp-c1ae4.appspot.com",
+    messagingSenderId: "322914022913",
+    appId: "1:322914022913:web:fcf7c4c7bd75fbdb37f200"
+  }
 };
